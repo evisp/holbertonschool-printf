aa4ea28c3778ee25dab99f7fcba6245bb3f09592
evisp <evisplaku@gmail.com>
2024-11-23 09:00:32 +0000

    modularize printf

--- _printf.c
+++ _printf.c
@@ -3,56 +3,65 @@
 #include <stddef.h>
 
 /**
- * _printf - Produces output according to a format
- * @format: Format string containing specifiers and text
- * 
- * Return: Number of characters printed (excluding the null byte)
+ * find_handler - Finds the appropriate handler for a specifier
+ * @format_char: The specifier character
+ * @specifiers: Array of specifier_t structs
+ *
+ * Return: Function pointer to the handler, or NULL if not found
  */
-int _printf(const char *format, ...)
+int (*find_handler(char format_char, specifier_t specifiers[]))(va_list)
 {
-    va_list args;
-    unsigned int i = 0, j = 0, flag = 0;
-    unsigned int len = 0;
+    unsigned int j = 0;
 
-    specifier_t specifiers[] = {
-        {"c", handle_char}, 
-        {"s", handle_string}, 
-        {NULL, NULL}
-    };
+    while (specifiers[j].specifier != NULL)
+    {
+        if (format_char == specifiers[j].specifier[0])
+            return (specifiers[j].handler);
+        j++;
+    }
+    return (NULL);
+}
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
-
-    va_start(args, format);
+/**
+ * process_format - Processes the format string and handles output
+ * @format: Format string
+ * @args: Argument list
+ * @specifiers: Array of specifier_t structs
+ *
+ * Return: Number of characters printed
+ */
+int process_format(const char *format, va_list args, specifier_t specifiers[])
+{
+    unsigned int i = 0;
+    int len = 0;
+    int (*handler)(va_list);
 
     while (format[i] != '\0')
     {
-        if (format[i] == '%' && format[i + 1] != '%')
+        if (format[i] == '%' && format[i + 1] != '\0')
         {
-            flag = 0;
-            j = 0;
-            while (specifiers[j].specifier != NULL)
+            if (format[i + 1] == '%')
             {
-                if (format[i + 1] == specifiers[j].specifier[0])
-                {
-                    len += specifiers[j].handler(args);
-                    flag = 1;
-                    i++;
-                    break;
-                }
-                j++;
-            }
-            if (!flag)
-            {
-                _putchar(format[i]);
+                _putchar('%');
                 len++;
+                i++;
             }
-        }
-        else if (format[i] == '%' && format[i + 1] == '%')
-        {
-            _putchar('%');
-            i++;
-            len++;
+            else
+            {
+                handler = find_handler(format[i + 1], specifiers);
+                if (handler)
+                {
+                    len += handler(args);
+                    i++;
+                }
+                else
+                {
+                    _putchar('%');
+                    _putchar(format[i + 1]);
+                    len += 2;
+                    i++;
+                }
+            }
         }
         else
         {
@@ -61,7 +70,32 @@ int _printf(const char *format, ...)
         }
         i++;
     }
+    return (len);
+}
 
+/**
+ * _printf - Produces output according to a format
+ * @format: Format string containing specifiers and text
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int len;
+
+    specifier_t specifiers[] = {
+        {"c", handle_char},
+        {"s", handle_string},
+        {NULL, NULL}
+    };
+
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
+
+    va_start(args, format);
+    len = process_format(format, args, specifiers);
     va_end(args);
+
     return (len);
 }
--- main.h
+++ main.h
@@ -1,23 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 
 /**
  * struct specifier_t - Struct for format specifiers and their handlers
- * @specifier: The format specifier (e.g., "c", "s", "d")
- * @handler: Pointer to the function that handles the specifier
+ * @specifier: The format specifier (e.g., "c", "s")
+ * @handler: Function pointer to the handler function for the specifier
  */
 typedef struct specifier_t
 {
-	char *specifier;
+    char *specifier;
     int (*handler)(va_list args);
 } specifier_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 
+int (*find_handler(char format_char, specifier_t specifiers[]))(va_list);
+int process_format(const char *format, va_list args, specifier_t specifiers[]);
+
 int handle_char(va_list args);
 int handle_string(va_list args);
-int handle_int(va_list args);
 
-#endif
+#endif 
